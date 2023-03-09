7be8366a2c439e291dccfc6c7e3871b0f0d08d6b
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-03-09 17:33:14 +0400

    complete task52

---
+++ task52/Program.cs
@@ -0,0 +1,50 @@
+﻿int[,] GetMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i,j] = new Random().Next(1,1000);
+        
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите колличество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите колличество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] resultMatrix = GetMatrix(m,n);
+
+void AverMatrix(int[,]resultMatrix)
+{
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        double sr_sum = 0;
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            sr_sum = sr_sum + resultMatrix[i, j];
+        }
+        sr_sum = sr_sum / resultMatrix.GetLength(0);
+        Console.WriteLine($"В столбце {j} среднее орифметическое равно {sr_sum}");
+    }
+}
+
+PrintMatrix(resultMatrix);
+AverMatrix(resultMatrix);
